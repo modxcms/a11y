3d71972bb0c7475caa1854735702e48951736ec7
Romain <romain@melting-media.com>
2014-09-10 13:41:08 +0200

    Base styling for default Revolution theme + Support for custom themes

--- core/components/verticalnavigation/elements/plugins/plugin.php
+++ core/components/verticalnavigation/elements/plugins/plugin.php
@@ -12,16 +12,26 @@
 $controller = $modx->controller;
 
 if ($modx->getOption('vnav.use_vnav')) {
-    // Load vertical menu xtype & Layout overrides
-    $vnav = $modx->getOption(
+    $base = $modx->getOption(
         'vnav.assets_url',
         null,
         $modx->getOption('manager_url') . 'assets/components/verticalnavigation/'
-    ) . 'js/vertical-menu.js';
+    );
+    // Base CSS
+    $controller->addCss($base . 'css/app.css');
+    $theme = $modx->getOption('manager_theme');
 
-    // @TODO : load CSS
+    if ($theme !== 'default') {
+        // Check for custom theme CSS
+        $file = "templates/{$theme}/css/vnav.css";
 
-    $controller->addJavascript($vnav);
+        if (file_exists($modx->getOption('manager_path') . $file)) {
+            $controller->addCss($modx->getOption('manager_url') . $file);
+        }
+    }
+
+    // JS
+    $controller->addJavascript($base . 'js/vertical-menu.js');
 }
 
 return '';
---
+++ sass.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+sass --watch _build/sass/app.scss:manager/components/verticalnavigation/css/app.css
