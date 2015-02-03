74cd67983c912ba6000973cbb719d9dce7de9d9f
Wayne Roddy <wayneroddy@Waynes-MacBook-Pro.local>
2015-02-03 14:15:48 -0500

    Vertical Navigation Plugin

---
+++ core/components/verticalnavigation/lexicon/en/default.inc.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Default English lexicon entries
+ */
+
+$_lang['setting_vnav.use_border'] = 'Use "border" navigation';
+$_lang['setting_vnav.use_border_desc'] = 'Whether or not to use vertical "border" navigation in the left area.';
+$_lang['setting_vnav.use_vnav'] = 'Use vertical navigation';
+$_lang['setting_vnav.use_vnav_desc'] = 'Whether or not to use vertical navigation in the left area.';
