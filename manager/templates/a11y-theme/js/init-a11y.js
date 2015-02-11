b01fd33e508576892b88870ae3dd1b8d253a6064
Wayne Roddy <wayneroddy@Waynes-MacBook-Pro.local>
2015-02-11 13:42:40 -0500

    Color Contrast Uberbar

--- manager/templates/a11y-theme/js/init-a11y.js
+++ manager/templates/a11y-theme/js/init-a11y.js
@@ -43,6 +43,42 @@ function initA11y() {
 	    topNavChildren[top_i].setAttribute("aria-haspopup", "true");
 	}
 	
+	/**
+	 * Handle vertical navigation assets
+	 *
+	 * @var modX $modx
+	 * @var array $scriptProperties
+	 *
+	 * @event OnManagerPageBeforeInit
+	 */
+	
+	/** @var modManagercontroller $controller */
+	$controller = $modx->controller;
+	
+	if ($modx->getOption('vnav.use_vnav')) {
+	    $base = $modx->getOption(
+	        'vnav.assets_url',
+	        null,
+	        $modx->getOption('manager_url') . 'assets/components/verticalnavigation/'
+	    );
+	    // Base CSS
+	    $controller->addCss($base . 'css/app.css');
+	    $theme = $modx->getOption('manager_theme');
+	
+	    if ($theme !== 'default') {
+	        // Check for custom theme CSS
+	        $file = "templates/{$theme}/css/vnav.css";
+	
+	        if (file_exists($modx->getOption('manager_path') . $file)) {
+	            $controller->addCss($modx->getOption('manager_url') . $file);
+	        }
+	    }
+	
+	    // JS
+	    $controller->addJavascript($base . 'js/vertical-menu.js');
+	}
+	
+	return '';
 	
 }// eof initA11y
 
