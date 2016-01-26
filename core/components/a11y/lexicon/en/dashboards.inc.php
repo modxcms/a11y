922c0c3832b5087ea06c972b191df018544b1f31
Ryan Thrash <ryan@modx.com>
2016-01-26 09:35:50 -0600

    Update Dashboard widget text to contain wrapping paragraph tag and add Theme Switcher description.

--- core/components/a11y/lexicon/en/dashboards.inc.php
+++ core/components/a11y/lexicon/en/dashboards.inc.php
@@ -1,19 +1,15 @@
 <?php
 
 $_lang['a11y.w_fontsize'] = 'Adjust Font Size';
-$_lang['a11y.w_fontsize_desc'] = 'Increase or decrease the Manager font size with these buttons.';
+$_lang['a11y.w_fontsize_desc'] = '<p>Use these buttons to increase or decrease the Manager font size.</p>';
 $_lang['a11y.increase'] = 'Increase';
 $_lang['a11y.decrease'] = 'Decrease';
 $_lang['a11y.w_dislexia'] = 'Enable Dyslexia Font';
-$_lang['a11y.w_dislexia_desc'] = 'The <a href="http://opendyslexic.org/" target="_blank">OpenDyslexic font</a> is for everyone with reading problems, including Dyslexia.';
+$_lang['a11y.w_dislexia_desc'] = '<p>The <a href="http://opendyslexic.org/" target="_blank">OpenDyslexic font</a> is for everyone with reading problems, including Dyslexia.</p>';
 $_lang['a11y.enabled'] = 'Enabled';
 $_lang['a11y.disabled'] = 'Disabled';
 $_lang['a11y.enable'] = 'Enable';
 $_lang['a11y.disable'] = 'Disable';
-
-$_lang['a11y.widget.dyslexia'] = 'Dyslexia';
-$_lang['a11y.widget.dyslexia_description'] = '';
-$_lang['a11y.widget.font_size'] = 'Font Size';
-$_lang['a11y.widget.font_size_description'] = '';
-$_lang['a11y.widget.switch_theme'] = 'Switch Theme';
-$_lang['a11y.widget.switch_theme_description'] = '';
+$_lang['a11y.w_switch_theme'] = 'Switch Theme';
+$_lang['a11y.w_switch_theme_desc'] = '<p>The "a11y" theme makes the MODX Manager accessible to assitive device users. Choose your theme using the select list.</p>';
+$_lang['a11y.tree_instructions'] = 'Tree menus. Use the tab-key to skip between Resources, Elements and Files. Press Enter to open or collapse a tree, and use the arrow keys to navigate the tree nodes. Press control-F10 on Windows and Linux, or command-F10 on Mac OS, to access the context menu for each tree node.';
