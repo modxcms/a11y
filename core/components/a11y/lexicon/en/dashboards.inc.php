903ab565758d988f0c4d4721900bb5a3bd2f288f
Ryan Thrash <ryan@modx.com>
2016-01-26 09:54:48 -0600

    Fix typo and more descriptive buttons
    Resolves #84

--- core/components/a11y/lexicon/en/dashboards.inc.php
+++ core/components/a11y/lexicon/en/dashboards.inc.php
@@ -2,14 +2,12 @@
 
 $_lang['a11y.w_fontsize'] = 'Adjust Font Size';
 $_lang['a11y.w_fontsize_desc'] = '<p>Use these buttons to increase or decrease the Manager font size.</p>';
-$_lang['a11y.increase'] = 'Increase';
-$_lang['a11y.decrease'] = 'Decrease';
-$_lang['a11y.w_dislexia'] = 'Enable Dyslexia Font';
-$_lang['a11y.w_dislexia_desc'] = '<p>The <a href="http://opendyslexic.org/" target="_blank">OpenDyslexic font</a> is for everyone with reading problems, including Dyslexia.</p>';
-$_lang['a11y.enabled'] = 'Enabled';
-$_lang['a11y.disabled'] = 'Disabled';
-$_lang['a11y.enable'] = 'Enable';
-$_lang['a11y.disable'] = 'Disable';
+$_lang['a11y.increase'] = '+ Font Size';
+$_lang['a11y.decrease'] = '- Font Size';
+$_lang['a11y.w_dyslexia'] = 'Enable OpenDyslexia';
+$_lang['a11y.w_dyslexia_desc'] = '<p><a href="http://opendyslexic.org/" target="_blank">OpenDyslexic</a> is a font for anyone with reading problems like Dyslexia.</p>';
+$_lang['a11y.enable'] = 'Use OpenDyslexia Font';
+$_lang['a11y.disable'] = 'Using OpenDyslexia Font—Revert to Default';
 $_lang['a11y.w_switch_theme'] = 'Switch Theme';
 $_lang['a11y.w_switch_theme_desc'] = '<p>The "a11y" theme makes the MODX Manager accessible to assitive device users. Choose your theme using the select list.</p>';
 $_lang['a11y.tree_instructions'] = 'Tree menus. Use the tab-key to skip between Resources, Elements and Files. Press Enter to open or collapse a tree, and use the arrow keys to navigate the tree nodes. Press control-F10 on Windows and Linux, or command-F10 on Mac OS, to access the context menu for each tree node.';
